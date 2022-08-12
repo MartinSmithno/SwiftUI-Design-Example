fe79864ca34464feb6e922299ad38393f12ae922
= <=>
2022-08-12 23:13:27 +0200

    Update Item and CellDesign

--- SwiftUI-Design-Example/Model/MenuItem.swift
+++ SwiftUI-Design-Example/Model/MenuItem.swift
@@ -6,16 +6,18 @@ class MenuItem: Identifiable {
     var name: String?
     var photo: String?
     var price: Float?
+    var count: Int = 0
     
     init() {
         
     }
     
-    init(id: String, name: String, photo: String, price: Float) {
+    init(id: String, name: String, photo: String, price: Float, count: Int) {
         self.id = id
         self.name = name
         self.photo = photo
         self.price = price
+        self.count = count
     }
     
 }
