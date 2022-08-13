395dbaecc3f922f4079e9665d3d3d920fa593c9d
= <=>
2022-08-13 06:31:39 +0200

    Update cell-design

--- SwiftUI-Design-Example/Model/CellDesign.swift
+++ SwiftUI-Design-Example/Model/CellDesign.swift
@@ -15,9 +15,9 @@ struct CellDesign: View {
                     .resizable()
                     .frame(width: 100, height: 100)
                 
-                VStack(alignment:.leading, spacing: 50){
+                VStack(alignment:.leading, spacing: 40){
                     Text(self.item.name!).bold().font(.title3)
-                    Text("\(String(format: "%.2f", self.item.price!))").foregroundColor(.blue)
+                    Text("\(String(format: "%.2f", self.item.price!)) $").foregroundColor(.blue)
                 }
                 
                 Spacer()
@@ -31,8 +31,7 @@ struct CellDesign: View {
                 
                 }
                 
-            }
+            }.frame(width: geometry.size.width, height: 150)
         }
-        
     }
 }
