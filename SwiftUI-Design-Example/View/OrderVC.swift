540558a00fd21c641ee0b1effb80ea8ea245da9d
= <=>
2022-08-19 22:45:02 +0200

    Update OrderVC and Add OrderCellDesign

--- SwiftUI-Design-Example/View/OrderVC.swift
+++ SwiftUI-Design-Example/View/OrderVC.swift
@@ -11,14 +11,33 @@ struct OrderVC: View {
     var body: some View {
         VStack(){
             
-            Toggle(isOn: $orderIsApproved) {
-                Text("Order Approvement")
-            }.toggleStyle(CheckboxStyle())
+            List(){
+                
+                ForEach(order){ item in
+                    
+                    OrderCellDesign()
+                    
+                }.frame(height: 140)
             
-            Toggle(isOn: $paymentIsApproved) {
-                Text("Payment Approvement")
-            }.toggleStyle(CheckboxStyle())
+                Toggle(isOn: $orderIsApproved) {
+                    Text("Order Approvement")
+                }.toggleStyle(CheckboxStyle())
+                
+                Toggle(isOn: $paymentIsApproved) {
+                    Text("Payment Approvement")
+                }.toggleStyle(CheckboxStyle())
+                
+            }
             
+            NavigationLink(destination: OrderVC(order: order)) {
+                Text("CONFIRM")
+                    .frame(minWidth: 0, maxWidth: 300)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(40)
+                    .font(.title)
+            }.navigationBarTitle("Order Confirmation")
         }
     }
 }
